a700b7440423cea041e0bad40a4d0495d0f98790
makifun <48549846+Makifun@users.noreply.github.com>
2021-01-17 01:05:23 +0100

    Update update-all.sh

--- update-all.sh
+++ update-all.sh
@@ -4,6 +4,7 @@ echo "[$0] ***** Pulling all images... *****"
 docker-compose pull
 echo "[$0] ***** Recreating containers if required... *****"
 docker-compose up -d
-echo "[$0] ***** Done. *****"
+echo "[$0] ***** Removing old, unused images... *****"
 docker image prune -f
+echo "[$0] ***** Done. *****"
 exit 0
